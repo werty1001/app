1ce82d68e7ce07a299ae9737de60a613c6a1cf71
werty1001 <werty1001@mail.ru>
2019-02-20 21:46:16 +0300

    Add some

--- blocks/develop/social/social.js
+++ blocks/develop/social/social.js
@@ -1,46 +1,46 @@
 
-	// Social share
+// Social share
 
-	(function () {
+(function () {
 
-		var share = {
+	var share = {
 
-			set: function () {
+		set: function () {
 
-				for ( var i = 0, el; el = this.els[i]; i++ ) {
+			for ( var i = 0, el; el = this.els[i]; i++ ) {
 
-					this.setLinks( el.querySelectorAll( '.social__link' ) )
-
-				}
-
-			},
-
-			setLinks: function (links) {
-
-				for ( var i = 0, link; link = links[i]; i++ ) {
-
-					link.href = link.href
-						.replace( /{url}/g, window.location )
-						.replace( /{title}/g, document.title )
-
-				}
-
-			},
-
-			init: function () {
-
-				this.els = document.querySelectorAll( '.social[data-share]' );
-
-				if ( this.els.length === 0 ) return this
-
-				this.set()
-
-				return this
+				this.setLinks( el.querySelectorAll( '.social__link' ) )
 
 			}
 
+		},
+
+		setLinks: function ( links ) {
+
+			for ( var i = 0, link; link = links[i]; i++ ) {
+
+				link.href = link.href
+					.replace( /{url}/g, window.location )
+					.replace( /{title}/g, document.title )
+
+			}
+
+		},
+
+		init: function () {
+
+			this.els = document.querySelectorAll( '.social[data-share]' );
+
+			if ( this.els.length === 0 ) return this
+
+			this.set()
+
+			return this
+
 		}
 
-		return share.init();
+	}
 
-	})();
+	return share.init();
+
+})();
